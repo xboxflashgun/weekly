94159e41ce70364f5a2df4e4b1d500c79d880efa
flashgun <flashgun@xboxstat.ru>
2024-11-03 17:45:15 +0000

    added: accuracy to table "mv_periods"

--- sql-templates/sql-next-period.sql
+++ sql-templates/sql-next-period.sql
@@ -9,15 +9,45 @@ begin;
 	refresh materialized view concurrently mv_%period%4;
 	refresh materialized view concurrently mv_%period%5;
 
-	insert into mv_periods(ts1,ts2,ts3,period) values(
-		date_trunc('%period%', now()-interval '2 %period%s'),
-		date_trunc('%period%', now()-interval '1 %period%'),
-		date_trunc('%period%', now()),
-		'%period%')
-	on conflict(period) do update set
-		ts1=date_trunc('%period%', now()-interval '2 %period%s'),
-		ts2=date_trunc('%period%', now()-interval '1 %period%'),
-		ts3=date_trunc('%period%', now());
+	with accur as (
+	    with tab2 as (
+	        with tab as (
+	            select 
+		            distinct utime 
+		            from presence 
+		            where utime>extract(epoch from now()-interval'1 %period%')::int 
+		            order by utime
+	        ) 
+	    select 
+	        utime,
+	        utime-lag(utime) over () as hole from tab as hole
+	    ) select
+	   	    date_trunc('%period%', now()-interval '2 %period%s') as ts1,
+		    date_trunc('%period%', now()-interval '1 %period%') as ts2,
+		    date_trunc('%period%', now()) as ts3,
+		    '%period%' as period,
+	        sum(hole)/extract(epoch from interval '1 %period%') as accuracy 
+	    from tab2 
+	    where hole>60
+	    group by ts1,ts2,ts3,period
+	    union 
+	    select
+	   	    date_trunc('%period%', now()-interval '2 %period%s') as ts1,
+		    date_trunc('%period%', now()-interval '1 %period%') as ts2,
+		    date_trunc('%period%', now()) as ts3,
+		    '%period%' as period,
+	        0 as accuracy 
+	    from tab2 
+	) insert into mv_periods(ts1,ts2,ts3,period,accuracy) (
+		select 
+			ts1,ts2,ts3,period,max(accuracy)
+		from accur
+		group by ts1,ts2,ts3,period
+	) on conflict(period) do update set
+		        ts1=date_trunc('%period%', now()-interval '2 %period%s'),
+		        ts2=date_trunc('%period%', now()-interval '1 %period%'),
+		        ts3=date_trunc('%period%', now()),
+		        accuracy=EXCLUDED.accuracy;
 
 commit;
 
