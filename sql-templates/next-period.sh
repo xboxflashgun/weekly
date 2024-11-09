4def62090ddb04ad3b1c66fa40f1e04955e588ae
flashgun <flashgun@xboxstat.ru>
2024-11-09 16:16:39 +0000

    added: export of raw data

--- sql-templates/next-period.sh
+++ sql-templates/next-period.sh
@@ -6,3 +6,6 @@ sed -e "s/%period%/$1/g" sql-next-period.sql > /tmp/next.$$.sql
 psql -Xq global -f /tmp/next.$$.sql
 rm /tmp/next.$$.sql
 
+sed -e "s/%period%/$1/g" sql-raw-data.sql > /tmp/raw.$$.sql
+psql --csv -Xf /tmp/raw.$$.sql | bzip2 > /var/www/html/xboxstat.com.ta.$1.csv.bz2
+rm /tmp/raw.$$.sql
