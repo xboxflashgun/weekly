c0563ff46662a1785b333c8733b0319a753f8a24
flashgun <flashgun@xboxstat.ru>
2024-11-09 15:15:22 +0000

    fixed: some bugs, css+js optimization

---
+++ devices.js
@@ -0,0 +1,58 @@
+////////////////
+// devices table
+function draw_devices() {
+
+	d3.select("#devtable tbody").selectAll("tr")
+	.data(Object.keys(devices).sort( (a,b) => devices[b].gamers - devices[a].gamers))
+	.join( enter => {
+		
+		var tr = enter.append('tr');
+		tr.attr('data-id', d => d);
+		tr.append('td').append('input').property('type', 'checkbox').attr('data-id', d => d).property('checked', d => devsel.has(d));
+		tr.append('td').text(d => devices[d].devname);
+		tr.append('td').text(d => devices[d].gamers);
+		tr.append('td').text(d => devices[d].games);
+		tr.append('td').text(d => compact(devices[d].avgh));
+		
+	}, update => {
+
+		update.select('td:nth-child(1)').attr('data-id', d => d.devid).property('checked', d => devsel.has(d.devid));
+		update.select('td:nth-child(2)').text(d => devices[d].devname);
+		update.select('td:nth-child(3)').text(d => devices[d].gamers);
+		update.select('td:nth-child(4)').text(d => devices[d].games);
+		update.select('td:nth-child(5)').text(d => compact(devices[d].avgh));
+	
+	}, exit => exit.remove()
+	);
+
+	d3.select("#devtable tbody").selectAll("tr").on('click', e => {		// click on row
+		if(e.target.tagName === 'TD') {
+		
+			var devid = e.target.parentNode.dataset.id;
+			if(devsel.has(devid))
+				devsel.delete(devid);
+			else
+				devsel.add(devid);
+			d3.select(e.target.parentNode).select('input').property('checked', devsel.has(devid));
+
+		} else {
+
+			var devid = e.target.dataset.id;
+			if(d3.select(e.target).property('checked'))
+				devsel.add(devid);
+			else
+				devsel.delete(devid);
+
+		}
+
+		read_data();
+
+	});
+
+	d3.select("#devselect").text( devsel.size ? `${devsel.size} / ${alldevices}` : "All" )
+		.on('click', e => d3.select("#devpopup").style("display", null)); 
+	d3.select("#devpopup .winclose").on('click', e => d3.select("#devpopup").style("display", "none"));
+
+}
+
+
--- genres.js
+++ genres.js
@@ -4,35 +4,50 @@ function draw_genre() {
 	.data(Object.keys(genres).sort( (a,b) => genres[b].gamers - genres[a].gamers))
 	.join( enter => {
 
-		var tr = enter.append('tr');
-		tr.append('td').append('input').property('type', 'radio').property('name','genreradio').property('checked', d => d === curgenre)
-			.attr('data-id', d => d);
-		tr.append('td').text(d => (d === "0") ? "All genres" : genres[d].genre);
+		var tr = enter.append('tr').attr('data-id', d => d);
+		var td = tr.append('td');
+		td.append('input').property('type', 'radio').property('name','genreradio').property('checked', d => d === curgenre);
+		td.append('span').text(d => (d === "0") ? "All genres" : genres[d].genre);
 		tr.append('td').text(d => genres[d].gamers);
 		tr.append('td').text(d => genres[d].games);
 		tr.append('td').text(d => compact(genres[d].avgh));
 
 	}, update => {
 
-		update.select('td:nth-child(1) input').property('checked', d => d === curgenre).attr('data-id', d => d);
-		update.select('td:nth-child(2)').text(d => (d === "0") ? "All genres" : genres[d].genre );
-		update.select('td:nth-child(3)').text(d => genres[d].gamers);
-		update.select('td:nth-child(4)').text(d => genres[d].games);
-		update.select('td:nth-child(5)').text(d => compact(genres[d].avgh));
+		update.attr('data-id', d => d);
+		update.select('td:nth-child(1) input').property('checked', d => d === curgenre);
+		update.select('td:nth-child(1) span').text(d => (d === "0") ? "All genres" : genres[d].genre );
+		update.select('td:nth-child(2)').text(d => genres[d].gamers);
+		update.select('td:nth-child(3)').text(d => genres[d].games);
+		update.select('td:nth-child(4)').text(d => compact(genres[d].avgh));
 
 	}, exit => exit.remove()
 
 	);
 
+	/*
 	d3.select("#genretable").selectAll("input").on('change', e => {
 
 		curgenre = e.target.dataset.id;
 		d3.select("#genressel").text( (curgenre === "0") ? "All" : genres[curgenre].genre);
 		draw_table();
 
-	});
+	}); */
 
 	d3.select("#genressel").text( (curgenre === "0") ? "All" : genres[curgenre].genre).on('click', () => d3.select("#genrepopup").style("display", null));
 	d3.select("#genrepopup .winclose").on('click', e => d3.select("#genrepopup").style("display", "none"));
+
+	d3.select("#genretable tbody").selectAll("tr").on('click', e => {
+
+		for(e = e.target; e.tagName !== 'TR'; e = e.parentNode)
+			;
+
+		curgenre = e.dataset.id;
+		console.log(curgenre);
+
+		d3.select(`#genretable tr[data-id="${curgenre}"] input`).property('checked', true);
+		draw_table();
+
+	});
 
 }
